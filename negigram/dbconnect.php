30c2128c6a43d32b2bf705d1a282a1b8b1d3bee6
knegi1109-lab <k.negi1109@gmail.com>
2025-08-13 14:58:50 +0900

    名前変更

--- negigram/dbconnect.php
+++ negigram/dbconnect.php
@@ -2,9 +2,9 @@
   //sql接続をあらかじめ作っておく
   function db()
   {
-      $dsn = 'mysql:dbname=******;host=localhost';
-      $dbUser = '******';
-      $dbPassword = '******';
+      $dsn = 'mysql:dbname=****;host=localhost';
+      $dbUser = '****';
+      $dbPassword = '********';
       $pdo = new PDO($dsn, $dbUser, $dbPassword, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
       
       return $pdo;
